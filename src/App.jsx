cd1fbba670cc7b96a0120f27a638a87db1922158
Shrutik Vankundre <shrutikvankundre1008@gmail.com>
2025-04-12 23:33:23 +0530

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,30 @@ function App() {
   return (
     <>
     <div class="title">
-      <h2 align="center">Car Wallpapers</h2>
+      <h2 align="center">Gear Head</h2>
     </div>
-      
+    
+    <body>
+    <main class="main">
+        <section>
+            <div>
+                <button class="btn">Home</button>
+            </div>
+            <div>
+                <button class="btn one">Collection</button>
+            </div>
+            <div>
+                <button class="btn two">About</button>
+            </div>
+            <div>
+                <button class="btn three">Contact</button>
+            </div>
+            {/* <div>
+                <button class="btn four">opacity</button>
+            </div> */}
+        </section>
+    </main>
+</body>
     </>
   )
 }
